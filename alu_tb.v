21cd5d6543c2d0fdb6a7efba92a87edcd00025a8
shreyasharma091874-cloud <shreyasharma091874@gmail.com>
2026-03-04 17:45:57 +0530

    Update alu_tb.v

--- alu_tb.v
+++ alu_tb.v
@@ -1,31 +1,23 @@
-module alu_tb;
-
-reg [7:0] A, B;
-reg [2:0] ALU_Sel;
-
-wire [7:0] ALU_Out;
-
-alu uut (
-    .A(A),
-    .B(B),
-    .ALU_Sel(ALU_Sel),
-    .ALU_Out(ALU_Out)
-);
-
 initial begin
 
 A = 8'd10;
 B = 8'd5;
 
 ALU_Sel = 3'b000; #10;
+$display("Addition = %d", ALU_Out);
+
 ALU_Sel = 3'b001; #10;
+$display("Subtraction = %d", ALU_Out);
+
 ALU_Sel = 3'b010; #10;
+$display("AND = %d", ALU_Out);
+
 ALU_Sel = 3'b011; #10;
+$display("OR = %d", ALU_Out);
+
 ALU_Sel = 3'b100; #10;
-ALU_Sel = 3'b101; #10;
+$display("XOR = %d", ALU_Out);
 
 $stop;
 
 end
-
-endmodule
